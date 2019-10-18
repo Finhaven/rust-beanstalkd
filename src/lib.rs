50a6be98c31918e5c5fd8fe1b6a3538f9e11529a
Evan Dickinson <evan@finhaven.com>
2019-10-17 17:22:23 -0700

    Make BeanstalkdError type public

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # Easy-to-use beanstalkd client for Rust (IronMQ compatible)
 
 pub use beanstalkd::Beanstalkd;
+pub use error::{BeanstalkdError, BeanstalkdResult};
 
 mod beanstalkd;
 mod commands;
